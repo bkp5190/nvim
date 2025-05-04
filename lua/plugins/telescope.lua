af3d3efa8735946b3f5727991b0073b3debde8c3
bpatel347 <bpatel347@gatech.edu>
2025-05-04 11:34:33 -0400

    Remove unused go files, modify tresitter and telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,30 @@
 return {
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-	config = function()
-		require('telescope').setup({
-			defaults = {
-				file_ignore_patterns = { '^.git/', '^.mypy_cache/', '^__pycache__/', '^output/', '^data/', '%.ipynb', '%.mod', '%.sum', '%.log' },
-				set_env = { COLORTERM = 'truecolor' },
-			},
-		})
-	end
-  },
-  {
-    'debugloop/telescope-undo.nvim',
-    dependencies = { 'nvim-telescope/telescope.nvim' },
-    config = function()
-      require('telescope').load_extension('undo')
-    end,
-  },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					file_ignore_patterns = {
+						"^.git/",
+						"^.mypy_cache/",
+						"^__pycache__/",
+						"^output/",
+						"^data/",
+						"%.ipynb",
+						"%.log",
+					},
+					set_env = { COLORTERM = "truecolor" },
+				},
+			})
+		end,
+	},
+	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("telescope").load_extension("undo")
+		end,
+	},
 }
